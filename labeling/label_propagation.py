attempt to fix normalization in LabProp + add multiple subgraphs

labeling/label_propagation.py
@@ -12,20 +12,37 @@ import networkx as nx
 import warnings
 from tqdm import tqdm
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    # shape (n_nodes, n_classes)
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    return e_x / e_x.sum(axis=1, keepdims=True)
+
+def log_softmax(x):
+    # shape (n_nodes, n_classes)
+    print("Logprobs:", x)
+    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+    res = np.log(e_x / e_x.sum(axis=1, keepdims=True))
+    assert np.all(~np.isnan(res))
+    return res
+
+
 class LabelProp:
     """ Adapted from LabelPropagation in sklearn """
-    def __init__(self, sg_size, inf_algo,
+    def __init__(self, sg_sizes, inf_algo,
                 max_iter=50, tol=1e-3):
         """Constructor.
         Arguments:
-            sg_size {int} -- size of subgraph to sample
-                             for running inf_algo
+            sg_sizes {list[int]} -- list of subgraph sizes to sample
+                                    for running inf_algo on
             inf_algo {Inference} -- Inference object
-                                 to run on subgraph
+                                    to run on subgraph
             max_iter {int} -- max number of propagation iterations
             tol {float} -- early stopping criterion
         """
-        self.sg_size  = sg_size
+        self.sg_sizes  = sg_sizes
         self.inf_algo = inf_algo  # already knows about the mode
                                   # (marginal/map)
         # set label prop params:
@@ -36,19 +53,25 @@ class LabelProp:
     def run_one(self, graph):
         n_nodes, n_classes = len(graph.W), 2
 
-        # choose a random subgraph graphical model
-        nodes = np.random.choice(n_nodes, min(self.sg_size, n_nodes), replace=False)
-        nodes = sorted(nodes)
-        sg = graph.get_subgraph_on_nodes(nodes)
-
-        # labeled_distr = self.inf_algo.run([graph])[0]
-        labeled_distr = self.inf_algo.run([sg])[0]
-        graph_matrix = graph.W
-        unlabeled = np.array([(i not in nodes) for i in range(n_nodes)])  #(y == -1)
-
-        # initialize distributions
         self.label_distributions_ = np.ones((n_nodes, n_classes)) * 1/n_classes
-        self.label_distributions_[nodes] = labeled_distr
+        rem_nodes = list(range(n_nodes))
+        for sz in self.sg_sizes:
+            # 1. sample sz nodes from remaining nodes
+            n_to_sample = min(sz, len(rem_nodes))
+            sampled_nodes = np.random.choice(rem_nodes, n_to_sample, replace=False)
+            sampled_nodes = sorted(sampled_nodes)
+            # 2. build subgraph graphical model:
+            sg = graph.get_subgraph_on_nodes(sampled_nodes)
+
+            # 3. label and record it
+            labeled_distr = self.inf_algo.run([sg])[0]
+            self.label_distributions_[sampled_nodes] = labeled_distr
+
+            # 4. remove nodes
+            rem_nodes = [nd for nd in rem_nodes if nd not in sampled_nodes]
+
+        graph_matrix = self._get_graph_matrix(graph.W)
+        unlabeled = np.array([(i in rem_nodes) for i in range(n_nodes)])  #(y == -1)
 
         y_static = np.copy(self.label_distributions_)
         y_static[unlabeled] = 0
@@ -61,25 +84,33 @@ class LabelProp:
                 break
 
             l_previous = self.label_distributions_
-            self.label_distributions_ = np.dot(graph_matrix, self.label_distributions_)
 
-            normalizer = np.sum(
-                self.label_distributions_, axis=1)[:, np.newaxis]
+            # TODO: test using logsoftmax normalization
+            # self.label_distributions_ = log_softmax(np.dot(graph_matrix, self.label_distributions_))
 
-            self._patch_normalizer(normalizer)
-            self.label_distributions_ /= normalizer
+            self.label_distributions_ = softmax(np.dot(graph_matrix, self.label_distributions_))
+            # normalizer = np.sum(
+            #     self.label_distributions_, axis=1)[:, np.newaxis]
+            # self._check_normalizer(normalizer)
+            # self.label_distributions_ /= normalizer
             self.label_distributions_ = np.where(unlabeled,
-                                                 self.label_distributions_,
-                                                 y_static)
+                                                self.label_distributions_,
+                                                y_static)
 
         else:
             warnings.warn(f'max_iter={self.max_iter} was reached without convergence.')
             self.n_iter_ += 1
 
         normalizer = np.sum(self.label_distributions_, axis=1)[:, np.newaxis]
-        self._patch_normalizer(normalizer)
+        self._check_normalizer(normalizer)
         self.label_distributions_ /= normalizer
+        self._check_output(self.label_distributions_)
         return self.label_distributions_
+
+        # TODO: from log scale
+        # output_distros = softmax(self.label_distributions_)
+        # self._check_output(output_distros)
+        # return output_distros
 
     def run(self, graphs, verbose=False):
         self.verbose = verbose
@@ -89,8 +120,12 @@ class LabelProp:
             res.append(self.run_one(graph))
         return res
 
-    def _patch_normalizer(self, normalizer, value=1e-6):
-        if not np.all(normalizer != 0.):
-            normalizer[normalizer == 0.] = value
+    def _get_graph_matrix(self, W):
+        return W
+        # return np.abs(W)
+
+    def _check_normalizer(self, normalizer):
         assert np.all(normalizer != 0.)
 
+    def _check_output(self, output):
+        assert np.all(output >= 0.) and np.all(output <= 1.), output

labeling/unittest_labeling.py
@@ -27,35 +27,29 @@ class TestInference(unittest.TestCase):
         labels = labelSG.partition_graph(graph, algorithm='igraph-label_propagation', verbose=True)
         labels = labelSG.partition_graph(graph, algorithm='igraph-optimal_modularity', verbose=True)
 
-
     def run_lbp_on_graph(self, graph):
         exact = get_algorithm("exact")("marginal")
 
         print("With subgraph of size 1")
-        lbp = LabelProp(1, exact)
+        lbp = LabelProp([1], exact)
         res = lbp.run([graph])
         true_res = exact.run([graph])
         mse_err = np.sqrt(np.sum(np.array(res) - np.array(true_res))**2)
         print(f"MSE error: {mse_err}")
 
         print("With subgraph of size 5")
-        lbp = LabelProp(5, exact)
+        lbp = LabelProp([5], exact)
         res = lbp.run([graph])
         true_res = exact.run([graph])
         mse_err = np.sqrt(np.sum(np.array(res) - np.array(true_res))**2)
         print(f"MSE error: {mse_err}")
 
         print("With subgraph of size 10")
-        lbp = LabelProp(10, exact)
+        lbp = LabelProp([10], exact)
         res = lbp.run([graph])
         true_res = exact.run([graph])
         mse_err = np.sqrt(np.sum(np.array(res) - np.array(true_res))**2)
         print(f"MSE error: {mse_err}")
-
-    def test_label_prop(self):
-        """ Testing marginal label_prop """
-        self.run_lbp_on_graph(self.graph_star)
-        self.run_lbp_on_graph(self.graph_fc)
 
     def run_tree_on_graph(self, graph):
         exact = get_algorithm("exact")("marginal")
@@ -66,13 +60,18 @@ class TestInference(unittest.TestCase):
         mse_err = np.sqrt(np.sum(np.array(res) - np.array(true_res))**2)
         print(f"MSE error: {mse_err}")
 
-    def test_tree_prop(self):
+    def test_label_prop(self):
+        """ Testing marginal label_prop """
+        self.run_lbp_on_graph(self.graph_star)
+        self.run_lbp_on_graph(self.graph_fc)
+
+    def _test_tree_prop(self):
         """ Testing tree-based generation """
         print("Trees:")
         self.run_tree_on_graph(self.graph_star)
         self.run_tree_on_graph(self.graph_fc)
 
-    def test_graph_cut(self):
+    def _test_graph_cut(self):
         """ Testing graph cut """
         self.run_lbp_subgraph(self.graph_barbell_50)
         self.run_lbp_subgraph(self.graph_cycle_50)