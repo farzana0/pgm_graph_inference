df4cff4741a8a834be0b4b4a1199645104486944
markcheung <markcheung7@gmail.com>
2019-04-30 20:59:15 -0400

    Updated comments.

--- run_sample.sh
+++ run_sample.sh
@@ -4,7 +4,7 @@
 # run as bash run_small.sh 
 
 echo -e "\tCreating train and test dataset with path structure and 9 nodes (small)"
-echo -e "\tNote you only need to run create_data and train.py once"
+echo -e "\tNote you only need to run create_data.py (for train and test dataset) and train.py once"
 python create_data.py --graph_struct path --size_range 9_9 \
                       --num 1300 --data_mode train --mode marginal --algo exact \
                       --verbose True
@@ -12,13 +12,13 @@ python create_data.py --graph_struct path --size_range 9_9 \
                       --num 300 --data_mode test --mode marginal --algo exact \
                       --verbose True
 
-echo -e "\tTraining dataset (see run_exps.py for possible train->test, and exp_helpers for their defintiions)"
-echo -e "\t For example, trees_medium contains "star": [15, 16, 17], and "path": [15, 16, 17] where 15,16,17 are number of nodes"   
+echo -e "\tTraining dataset (see run_exps.py for possible types of inferences, and exp_helpers for their defintiions)"
+echo -e "\tFor example, trees_medium contains "star": [15, 16, 17], and "path": [15, 16, 17] where 15,16,17 are number of nodes"   
 
 python train.py --train_set_name path_small --mode marginal --epochs 5 --verbose True
 
-echo -e "Generate plots for bp, MCMC, and GNN for the given experiment in experiments folder and saved the output in experiments/saved_exp_res"
+echo -e "\tGenerate plots for bp, mcmc, and gnn for the given experiment in experiments folder and saved the output in experiments/saved_exp_res"
 python ./experiments/run_exps.py --exp_name in_sample_path
 
-echo -e "compute MAP for bp, MCMC, and gnn"
+echo -e "\tCompute MAP for bp, mcmc, and gnn"
 python ./experiments/saved_exp_res/compute_MAP_accuracy.py --data_file ./experiments/saved_exp_res/res_path_small_path_small.npy
